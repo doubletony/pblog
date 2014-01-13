4e5a653a6f7da632a5d0e0959e83203f5216f2dd
unknown <yuawang@doubletony.redmond.corp.microsoft.com>
2014-01-13 11:11:50 -0800

    Add header into newDoodle.py

--- newDoodle.py
+++ newDoodle.py
@@ -1,8 +1,14 @@
 import glob
-
+import datetime
+header = """Title:
+Author: doubletony
+Date: TODAY
+"""
+header = header.replace("TODAY", datetime.datetime.today().strftime("%d %B %Y"))
 allDoodle = glob.glob("doodle*.md")
 newId =  len(allDoodle) + 1
 newDoodleName = "doodle"+str(newId).zfill(4)+".md"
 print newDoodleName
 f = open(newDoodleName, 'w+')
+f.write(header)
 f.close()
