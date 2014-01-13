4e43619a5ca3f1eac3dc7998aa90f6028996d52a
doubletony <doubletony@gmail.com>
2014-01-13 13:24:01 -0800

    Add showwokring

---
+++ showworking.py
@@ -0,0 +1,15 @@
+import glob
+import datetime
+allDoodle = glob.glob("doodle*.md")
+allHTML = glob.glob("doodle*.html")
+
+ret = ""
+for i in range(1, len(allDoodle)+1):
+    HTMLName = "doodle"+str(i).zfill(4)+".html"
+    if HTMLName not in allHTML:
+        ret += "doodle"+str(i).zfill(4)+".md\n"
+
+if ret == "":
+    print "No working item."
+else:
+    print ret
